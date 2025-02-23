f4688b5c1cad534561e3574f2f8e1ddbce74eb11
王虛白 <18016038327@189.cn>
2025-02-23 22:18:55 +0800

    chore: add nix default package

--- flake.nix
+++ flake.nix
@@ -13,26 +13,52 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        common = with pkgs; {
+          buildInputs = [
+            wayland
+            librime
+            libxkbcommon
+          ];
+          RIME_INCLUDE_DIR = "${librime}/include";
+          RIME_LIB_DIR = "${librime}/lib";
+        };
       in
       {
+        # 包
+        packages.default =
+          with pkgs;
+          rustPlatform.buildRustPackage (
+            {
+              pname = "wlrime";
+              version = "0.1.0";
+              src = ./.;
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                outputHashes = {
+                  "rime-api-0.1.0" = "sha256-VUhvKzC6sgPJidQ9bMLJvu3xZYqkThvGzzVsJUqn0rw=";
+                };
+              };
+              nativeBuildInputs = [
+                pkg-config
+                rustPlatform.bindgenHook
+              ];
+            }
+            // common
+          );
+
+        # 開發環境
         devShells.default =
           with pkgs;
-          mkShell {
-            packages = [ rust-bindgen ];
-            buildInputs = [
-              wayland
-              librime
-              libxkbcommon
-            ];
-            nativeBuildInputs = [
-              pkg-config
-              rustup
-              rustPlatform.bindgenHook
-            ];
-
-            RIME_INCLUDE_DIR = "${librime}/include";
-            RIME_LIB_DIR = "${librime}/lib";
-          };
-      }
+          mkShell (
+            {
+              nativeBuildInputs = [
+                pkg-config
+                rustup
+                rustPlatform.bindgenHook
+              ];
+            }
+            // common
+          );
+      } 
     );
 }
